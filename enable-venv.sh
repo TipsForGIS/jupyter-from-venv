fe9ee6ef5d2ba5725701343929e719bb723c8c30
Ahmad Omar Aburizaiza - Tips For GIS <67890725+TipsForGIS@users.noreply.github.com>
2020-09-13 11:45:27 -0400

    Create enable-venv.sh

---
+++ enable-venv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+###########################################
+# assuming you have Jupyter Lab installed # 
+# and you created a myvenv env            #
+# and you are in the path where myvenv is #
+###########################################
+
+# install ipykernel
+pip install --user ipykernel
+
+# add your venv to jupter
+python3 -m ipykernel install --user --name=myenv
+
+# if you see this message: Installed kernelspec myenv in /Users/../Library/Jupyter/kernels/myenv
+# this means the installation is successful 
+# and under this path, you should see kernel.json explaining the settings for your venv
+
+# to list kernels (global python + venv(s))
+jupyter kernelspec list
+
+# to remove a kernel
+jupyter kernelspec uninstall myenv
